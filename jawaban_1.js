e0f6e1a0a3bfafcdb487c9e518441aa5d4035567
limmi26 <kisamakiazuel@gmail.com>
2021-09-21 09:06:01 +0700

    perubahan 1

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,97 @@
 // Jawaban Soal 1 ada di bawah ini
+
+// objek 1
+const motor = {
+    motorName: "Aerox",
+    motorModel: 155,
+    motorColor: "White",
+    motorBerat: "125kg",
+    motorKondisi: true,
+    motorDetail: ['Merek','Kapasitas Bahan Bakar','Harga OTR','DiMensi'],
+
+    kondisiMotor: function() {
+    if(motorKondisi === true){
+    return 'Motor Kondisi Baru'
+    } else {
+    return 'Motor Kondisi Rusak'
+    }
+    }
+};
+
+console.log(motor); //hasil
+
+// objek 2
+const mobil = {
+    mobilName: "CR-V",
+    mobilColor: "White",
+    mobilRilis: 2021,
+    mobilKondisi: true,
+    mobilDetail: ['Merek','Kapasitas Bahan Bakar','Harga OTR','DiMensi'],
+
+    kondisiMobil: function() {
+    if(mobilKondisi === true){
+    return 'Mobil Kondisi Baru'
+    } else {
+    return 'Mobil Kondisi Rusak'
+    }
+    }
+};
+
+console.log(mobil); //hasil
+
+// objek 3
+const sepeda = {
+    sepedaName: "Fixie",
+    sepedaModel: "dropbar",
+    sepedaColor: "Black",
+    sepedaKondisi: true,
+    sepedaDetail: ['Merek','Kapasitas','Harga OTR','DiMensi'],
+
+    kondisiSepeda: function() {
+    if(sepedaKondisi === true){
+    return 'Sepeda Kondisi Baru'
+    } else {
+    return 'Sepeda Kondisi Rusak'
+    }
+    }
+};
+
+console.log(sepeda); //hasil
+
+// objek 4
+const mobil = {
+    mobilName: "CR-V",
+    mobilColor: "White",
+    mobilRilis: 2021,
+    mobilKondisi: true,
+    mobilDetail: ['Merek','Kapasitas Bahan Bakar','Harga OTR','DiMensi'],
+
+    kondisiMobil: function() {
+    if(mobilKondisi === true){
+    return 'Mobil Kondisi Baru'
+    } else {
+    return 'Mobil Kondisi Rusak'
+    }
+    }
+};
+
+console.log(mobil); //hasil
+
+// objek 5
+const mobil = {
+    mobilName: "CR-V",
+    mobilColor: "White",
+    mobilRilis: 2021,
+    mobilKondisi: true,
+    mobilDetail: ['Merek','Kapasitas Bahan Bakar','Harga OTR','DiMensi'],
+
+    kondisiMobil: function() {
+    if(mobilKondisi === true){
+    return 'Mobil Kondisi Baru'
+    } else {
+    return 'Mobil Kondisi Rusak'
+    }
+    }
+};
+
+console.log(mobil); //hasil
