e815840e346cbaae032bf85a4f62fe302da7fa9f
pthabot <prima.fams@gmail.com>
2021-09-21 09:19:07 +0700

    pbo-uts-ganjil-2021 prima

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,14 @@
 // Jawaban Soal 1 ada di bawah ini
+
+// Cara membuat object instance
+
+let hewanMacan = new Hewan("Macan", "Orange", 310, 10000000);
+hewanMacan.cekKondisiBagus(true);
+console.log(hewanMacan);
+
+let hewanMacan = new Hewan("Gajah", "Hitam", 4000, 10000000);
+hewanMacan.cekKondisiBagus(true);
+console.log(hewanGajah);
+
+console.log('ini hewan macan =>', hewanMacan);
+console.log('ini hewan gajah =>', hewanGajah);
