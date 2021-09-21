84c1f8ca39e235a1b983b596c0651b75cd58974b
sulthanarif <msarizky201@gmail.com>
2021-09-21 09:14:10 +0700

    perubahan 1

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,18 @@
 // Jawaban Soal 1 ada di bawah ini
+
+class hewan {
+    constructor(nama, ras, usia, kondisi, kaki) {
+        this.nama = nama;
+        this.ras = ras;
+        this.usia =usia;
+        this.kondisi = kondisi;
+        this.kaki = kaki;
+    }
+    cekKondisi(){
+        if(this.kondisi === true){
+            return `${this.nama} sehat`
+        } else{
+            return `sakit`
+        }
+    }
+}
