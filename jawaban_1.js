dd25547f244391ef881d4c4d0c013549efdd649e
gabriellenatt <gabriellenatasha525@gmail.com>
2021-09-21 09:14:55 +0700

    Ulangan belum selesai

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,17 @@
 // Jawaban Soal 1 ada di bawah ini
+class manusia {
+    constructor(nama, umur, warnaRambut, pekerjaan, asal) {
+        this.nama = nama;
+        this.umur = umur;
+        this.warnaRambut = warnaRambut;
+        this.pekerjaan = pekerjaan;
+        this.asal = asal;
+    }
+    cekUmur() {
+        if (this.umur >= 21) {
+            return 'Sudah Dewasa';
+        } else {
+            return 'Masih Kecil'
+        }
+    }
+}
