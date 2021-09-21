00c8c32c729010a54240e68841ea19b317f0fb72
maxdesanta <maxdesanta1@gmail.com>
2021-09-21 09:14:40 +0700

    belum selesai

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,17 @@
 // Jawaban Soal 1 ada di bawah ini
+class kendaraan {
+    constructor(nama, platNomor, kondisi, roda, type) {
+        this.nama = nama;
+        this.platNomor = platNomor;
+        this.kondisi = kondisi;
+        this.roda = roda;
+        this.type = type;
+    }
+    cekKondisi() {
+        if (this.kondisi === true) {
+            return `${this.nama} ini dalam kondisi bagus`;
+        } else {
+            return `${this.nama} ini dalam kondisi bekas`;
+        }
+    }
+}
